e6f990265d72f2e78a689062631ebcc2e3a3cd0c
Chris <chris35469@gmail.com>
2025-02-09 16:48:51 -0600

    update cache settings

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,25 +1,8 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-
+const { signal } = new AbortController()
 export async function POST(request: Request) {
     try {
-        // console.log(initializeApp)
-        // const data = await getData("front-page");
-        // console.log(data);
-
-        /*
-        const API_KEY = process.env.API_KEY;
-        const ENDPOINT = process.env.ENDPOINT;
-        const QUERY = process.env.QUERY;
-        const response = await fetch(`${ENDPOINT}?${QUERY}=${API_KEY}`);
-        const data = await response.json();
-        console.log(Object.keys(data).length);
-        */
-        // const API_KEY = process.env.API_KEY;
-        // const body = await request.json();
-        // // console.log(body);
-        // const { tokenAddress, rating } = body;
-        // console.log("Sending tokenAddress, rating", tokenAddress, rating);
         const body = await request.json();
         const response = await axios.get('https://w3bbiegames2.xyz/predict', {
             params: {
@@ -27,12 +10,6 @@ export async function POST(request: Request) {
                 key: "sendit"
             }
         });
-
-        // const response = await axios.get('https://w3bbiegames2.xyz/predict', {
-        //     address: "Hjw6bEcHtbHGpQr8onG3izfJY5DJiWdt7uk2BfdSpump",
-        //     key: "sendit"
-        // });
-
 
         return NextResponse.json(response.data);
     } catch (error) {
